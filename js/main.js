5c6af049c3790e91f35c221e33c05492bdf62e19
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-11-25 22:11:55 +0100

    Add the javascript file

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let randNum = Math.floor(Math.random() * 12 + 1);
+document.getElementById(
+  "position"
+).innerHTML = `Jolly Roger Position is: ${randNum}`;
+function boom(btn) {
+  alert(btn);
+}
